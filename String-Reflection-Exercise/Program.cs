7c36f4f8c26163135576f29b43ea786704fe5c40
roaa-al-homsi <roaakokalhomsi@gmail.com>
2025-03-18 18:15:41 +0300

    Appearing all public and instance methods in string library using concept REFLECTION

---
+++ String-Reflection-Exercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Training_Reflection
+{
+    internal class Program
+    {
+        public static string GetParameters(ParameterInfo[] parameters)
+        {
+            return string.Join(",", parameters.Select(p => $"{p.ParameterType} {p.Name}"));
+        }
+        static void Main(string[] args)
+        {
+            //get the assembly containing the system.string type.
+            Assembly assembly = typeof(string).Assembly;
+
+            //get the system string type.
+            Type stringType = assembly.GetType("System.String");
+
+            if (stringType != null)
+            {
+                Console.WriteLine("Methods of the system.string class..");
+
+                var stringMethods = stringType.GetMethods(BindingFlags.Public | BindingFlags.Instance).OrderBy(method => method.Name);
+                foreach (var method in stringMethods)
+                {
+                    Console.WriteLine($"\t{method.ReturnType} {method.Name} ({GetParameters(method.GetParameters())}) ");
+                }
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
